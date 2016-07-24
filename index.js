2b2da8522874795b6113ad3cb592394082aacfe0
Vladimir Tyalo <bobtial@inbox.ru>
2016-07-24 17:48:12 +0400

    add routes

---
+++ index.js
@@ -0,0 +1,40 @@
+(function () {
+  "use strict"
+
+  var express = require('express');
+  var app = express();
+  var path = require("path");
+
+  app.set("port", (process.env.PORT || 8080));
+
+  app.use(express.static(path.join(__dirname, 'build')));
+
+
+  app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'html');
+
+  app.set("views", path.join(__dirname,"/build"));
+
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
+
+
+  app.get("/index", function (req, res) {
+    res.redirect("/");
+  });
+
+  app.get("/resume", function (req, res) {
+    res.send("Not implemented yet");
+  });
+
+
+  app.use(function (req, res) {
+    res.sendStatus(404);
+  });
+
+  app.listen(app.get("port"), function () {
+    console.log("Node app is running on port ", app.get("port"));
+  });
+
+})();
