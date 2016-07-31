1fc6f621dca5026483141c6f769a7b762587222b
Vladimir Tyalo <bobtial@inbox.ru>
2016-07-31 14:53:56 +0400

    resume browser compatibility fix

---
+++ js/index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by vladimir on 7/31/16.
+ */
+(function () {
+    "use strict"
+
+  var burgerBtn = document.querySelector(".header__menu-icon");
+  var modalMenu = document.querySelector(".header__menu-list");
+
+  console.log("Hello");
+
+
+  initMenu();
+  addListeners();
+
+  function initMenu() {
+    toggleMenu();
+  }
+
+  function addListeners() {
+    burgerBtn.addEventListener("click", function(ev) {
+      ev.preventDefault();
+      toggleMenu();
+    });
+  }
+
+  function toggleMenu() {
+    burgerBtn.classList.toggle("header__menu-icon--close");
+    modalMenu.classList.toggle("header__menu-list--closed");
+  }
+
+})();
